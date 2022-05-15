3315c268a566bd4a3340769411d657e5536853b6
sudip011 <sudip.rout007@gmail.com>
2022-05-15 15:07:54 +0530

    Committing GUI_Intro

---
+++ src/com/SudipJavaCodes/GUI_Intro.java
@@ -0,0 +1,17 @@
+package com.SudipJavaCodes;
+import javax.swing.JOptionPane;
+
+public class GUI_Intro {
+    public static void main(String []args) {
+        //Scanner sc=new Scanner(System.in);
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height in cm"));
+        JOptionPane.showMessageDialog(null, "You are "+height+" cm tall");
+
+    }
+}
