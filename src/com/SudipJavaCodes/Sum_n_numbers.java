2344c4b5e9cd2dbff69c21ac42668bed7ed505d4
SudipRout <sudip.rout007@gmail.com>
2022-07-11 12:56:47 +0530

    Committing Sum_n_numbers

---
+++ src/com/SudipJavaCodes/Sum_n_numbers.java
@@ -0,0 +1,17 @@
+package com.SudipJavaCodes;
+
+import java.util.*;
+public class Sum_n_numbers {
+    public static void Main(String args[]) {
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum of first n numbers are: "+sum);
+    }
+}
+
