106ffee894fa3a774cf50e6509cec23ef1ba8333
SudipRout <sudip.rout007@gmail.com>
2022-07-12 14:09:52 +0530

    Committed Multiple_Table

---
+++ src/com/SudipJavaCodes/Multiple_Table.java
@@ -0,0 +1,16 @@
+package com.SudipJavaCodes;
+
+import java.util.Scanner;
+
+public class Multiple_Table {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("The multiplication table of "+n+" is here:");
+        for (int i=1; i<11; i++) {
+            System.out.println(i*n);
+        }
+    }
+}
