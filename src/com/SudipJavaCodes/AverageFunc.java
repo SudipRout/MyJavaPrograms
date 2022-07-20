f13e47b3359c1beee88213056359238c5428db6e
SudipRout <sudip.rout007@gmail.com>
2022-07-20 18:03:24 +0530

    Committed AverageFunc

--- src/com/SudipJavaCodes/AverageFunc.java
+++ src/com/SudipJavaCodes/AverageFunc.java
@@ -9,10 +9,11 @@ public class AverageFunc {
         double a = sc.nextInt();
         double b = sc.nextInt();
         double c = sc.nextInt();
-        
-        System.out.println("The average of the three numbers are: " +printAverage(a, b, c));
+
+        printAverage(a, b, c);
     }
-    public static double printAverage(double a, double b, double c) {
-        return (a+b+c)/3;
+    public static void printAverage(double a, double b, double c) {
+        double avg = (a+b+c)/3;
+        System.out.println("The average of the three numbers are: "+avg);
     }
 }
